db310bc6fbb4f6da232243ae7d84e32afe6512ca
couragor <couragor@gmail.com>
2017-03-12 21:31:03 +0700

    edit

--- utility.py
+++ utility.py
@@ -1,12 +1,6 @@
-# Borrowed some code from : 
-# https://www.udacity.com/course/machine-learning-for-trading--ud501
-# http://matplotlib.org/examples/pylab_examples/finance_demo.
 import os
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.dates import DateFormatter, WeekdayLocator,DayLocator, MONDAY
-from matplotlib.finance import date2num, candlestick_ohlc
+import pandas as pd
 
 def changeColumnName(df, postfix):
 	df.columns = [ '{}_{}'.format(sym, postfix) for sym in df.columns ]
@@ -47,7 +41,7 @@ def loadStockData(symbols, dates, column_name, base_dir):
 	 
 	return df
 
-default_path="output_csv"
+default_path="sec_csv"
 
 def loadPriceData(symbols, dates,  base_dir=default_path):
 	return loadStockData(symbols, dates, '<CLOSE>', base_dir)
@@ -61,7 +55,7 @@ def load_OHLCV(symbol, dates,
 	"""Read stock data (adjusted close) for given symbols from CSV files."""
 	df_main = pd.DataFrame(index=dates)	# empty data frame that has indexs as dates
 	
-	if '<DTYYYYMMDD>' not in column_names:  # add SET for reference, if absent
+	if '<DTYYYYMMDD>' not in column_names:  
 		column_names = np.append(['<DTYYYYMMDD>'],column_names)
 		
 	csv_file = os.path.join(base_dir, "{}.csv".format(symbol)) 
@@ -79,46 +73,11 @@ def load_OHLCV(symbol, dates,
 	
 	return df_main
 
-def loadStockQuotes(symbol, dates, base_dir=default_path):
+def loadStockQuotes(symbol, dates):
 	col_names=['<OPEN>', '<CLOSE>', '<HIGH>', '<LOW>']
-	df = loadOneStockData(symbol, dates, column_names=col_names)
+	df = load_OHLCV(symbol, dates, column_names=col_names,  base_dir=default_path)
 	
 	#quotes = [ (date, open, close, high, low), .....]
 	quotes = df.to_records(convert_datetime64=True).tolist() 
 	quotes = [ (date2num(d), o, c, h, l) for d,o,c,h,l in quotes  ]	
 	return quotes
-	
-def plotStock(df, columns, start_index, end_index, title="Selected data"):
-	"""Plot the desired columns over index values in the given range."""	
-	df_plot = df.ix[start_index:end_index, columns]
-	
-	"""Plot stock prices with a custom title and meaningful axis labels."""
-	ax = df_plot.plot(title=title, fontsize=12)
-	ax.set_xlabel("Date")
-	ax.set_ylabel("Price")
-	plt.show()
-
-def plotCandlestick(symbol, start_index, end_index, title="Selected data"):
-	dates = pd.date_range(start_index, end_index)	
-	quotes = loadStockQuotes(symbol, dates)		
-	
-	mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
-	alldays = DayLocator()              	# minor ticks on the days
-	weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
-	dayFormatter = DateFormatter('%d')      # e.g., 12
-
-	fig, ax = plt.subplots()
-	fig.subplots_adjust(bottom=0.2)
-	ax.xaxis.set_major_locator(mondays)
-	ax.xaxis.set_minor_locator(alldays)
-	ax.xaxis.set_major_formatter(weekFormatter)
-	#ax.xaxis.set_minor_formatter(dayFormatter)
- 
-	#plot_day_summary(ax, quotes, ticksize=3)
-	candlestick_ohlc(ax, quotes, width=0.6)
-
-	ax.xaxis_date()
-	ax.autoscale_view()
-	plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
-
-	plt.show()
